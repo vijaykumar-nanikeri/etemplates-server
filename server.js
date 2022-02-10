42e3f50e1af64d364ea2add451dde4c63c0d83c6
vijaykumar-nanikeri <90862488+nvijaykumar999@users.noreply.github.com>
2022-02-10 05:33:12 +0530

    eTemplates: UI Build.

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "bin/ui")));
 
 app.use("/app", appRouter);
 app.use("/login", authRouter);
