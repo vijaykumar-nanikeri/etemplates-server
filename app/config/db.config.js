6c10c4cdc7c0e7fdb00b92effce15d919d390e68
vijaykumar-nanikeri <90862488+nvijaykumar999@users.noreply.github.com>
2022-02-10 06:58:58 +0530

    eTemplates: DB Config.

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,15 @@
+// Host
 module.exports = {
-	HOST: "localhost",
-	USER: "root",
-	PASSWORD: "",
-	DB: "etemplates"
+	HOST: "us-cdbr-east-05.cleardb.net",
+	USER: "bc6f25e552b50a",
+	PASSWORD: "ee2ee8e8",
+	DB: "heroku_58c7324a67a7ae0"
 };
+
+// Local
+// module.exports = {
+// 	HOST: "localhost",
+// 	USER: "root",
+// 	PASSWORD: "",
+// 	DB: "etemplates"
+// };
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "bin/ui")));
+app.use(express.static(path.join(__dirname, "bin/src")));
 
 app.use("/app", appRouter);
 app.use("/login", authRouter);
